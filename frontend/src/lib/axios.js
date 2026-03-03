e261997e2124f1e2fa8ec93319d3c2ba08125aa6
EliteHaze <chmcs2026.limesh.chaudhari@gmail.com>
2026-03-03 20:26:08 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/exercises",
+  baseURL: "https://gymmate-backend-dkgv.onrender.com",
 });
 
 export default api;
